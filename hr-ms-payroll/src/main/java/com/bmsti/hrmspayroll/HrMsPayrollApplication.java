26ef972abbba2c9780d09c2fdb457d7ba246abe2
angelobms <angelobms@gmail.com>
2022-01-20 09:50:39 -0300

    Created hr-ms-worker project.

---
+++ hr-ms-payroll/src/main/java/com/bmsti/hrmspayroll/HrMsPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.bmsti.hrmspayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMsPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMsPayrollApplication.class, args);
+    }
+
+}
---
+++ hr-ms-payroll/src/test/java/com/bmsti/hrmspayroll/HrMsPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmsti.hrmspayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrMsPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

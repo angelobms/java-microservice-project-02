090f1ff720dedd37f7027f767579bb7bee4d920f
angelobms <angelobms@gmail.com>
2022-01-19 21:26:40 -0300

    Implemented hr-ms-worker.`

---
+++ hr-ms-worker/src/main/java/com/bmsti/hrmsworker/HrMsWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.bmsti.hrmsworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMsWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMsWorkerApplication.class, args);
+    }
+
+}
---
+++ hr-ms-worker/src/test/java/com/bmsti/hrmsworker/HrMsWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmsti.hrmsworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrMsWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

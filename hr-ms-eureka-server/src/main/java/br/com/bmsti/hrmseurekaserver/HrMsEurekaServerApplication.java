0dede5f7e6be9a5e4caafa507a8c249a6a716c70
angelobms <angelobms@gmail.com>
2022-01-26 20:50:49 -0300

    Created project hr-ms-eureka-server

---
+++ hr-ms-eureka-server/src/main/java/br/com/bmsti/hrmseurekaserver/HrMsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.bmsti.hrmseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMsEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMsEurekaServerApplication.class, args);
+    }
+
+}
---
+++ hr-ms-eureka-server/src/test/java/br/com/bmsti/hrmseurekaserver/HrMsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bmsti.hrmseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrMsEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

397630bc1fa058d4de2a12eacfede15846b22266
angelobms <angelobms@gmail.com>
2022-01-26 21:18:00 -0300

    Congigured eureka clients

--- hr-ms-payroll/src/main/java/com/bmsti/hrmspayroll/HrMsPayrollApplication.java
+++ hr-ms-payroll/src/main/java/com/bmsti/hrmspayroll/HrMsPayrollApplication.java
@@ -2,10 +2,12 @@ package com.bmsti.hrmspayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "rh-ms-worker")
+@EnableEurekaClient
+//@RibbonClient(name = "rh-ms-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrMsPayrollApplication {
--- hr-ms-worker/src/main/java/com/bmsti/hrmsworker/HrMsWorkerApplication.java
+++ hr-ms-worker/src/main/java/com/bmsti/hrmsworker/HrMsWorkerApplication.java
@@ -2,7 +2,9 @@ package com.bmsti.hrmsworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrMsWorkerApplication {
 

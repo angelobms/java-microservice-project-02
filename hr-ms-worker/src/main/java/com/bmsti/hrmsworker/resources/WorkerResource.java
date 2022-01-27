95a6a78592e36aea80472446a0caf24883165f6c
angelobms <angelobms@gmail.com>
2022-01-26 22:46:39 -0300

    Configured zuul timeout

--- hr-ms-worker/src/main/java/com/bmsti/hrmsworker/resources/WorkerResource.java
+++ hr-ms-worker/src/main/java/com/bmsti/hrmsworker/resources/WorkerResource.java
@@ -40,11 +40,11 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-        /*try {
+        try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
 
         logger.info("Port = " + environment.getProperty("local.server.port"));
 

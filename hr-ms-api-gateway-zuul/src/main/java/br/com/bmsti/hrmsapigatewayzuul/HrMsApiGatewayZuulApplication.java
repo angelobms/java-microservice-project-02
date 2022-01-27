c2d1fe76811ccb7f43886bc42d1f3257bc643569
angelobms <angelobms@gmail.com>
2022-01-26 22:03:50 -0300

    Added project hr-ms-api-gateway-zuul

---
+++ hr-ms-api-gateway-zuul/src/main/java/br/com/bmsti/hrmsapigatewayzuul/HrMsApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.bmsti.hrmsapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMsApiGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMsApiGatewayZuulApplication.class, args);
+    }
+
+}

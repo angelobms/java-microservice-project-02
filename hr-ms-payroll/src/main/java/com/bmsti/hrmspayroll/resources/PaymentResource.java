e89955af871fae03e3459a1d760dcd52b82b752b
angelobms <angelobms@gmail.com>
2022-01-26 21:56:12 -0300

    Added hystrix fault tolerance

--- hr-ms-payroll/src/main/java/com/bmsti/hrmspayroll/resources/PaymentResource.java
+++ hr-ms-payroll/src/main/java/com/bmsti/hrmspayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.bmsti.hrmspayroll.resources;
 
 import com.bmsti.hrmspayroll.entities.Payment;
 import com.bmsti.hrmspayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +21,16 @@ public class PaymentResource {
         this.paymentService = paymentService;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Marcelo", 500.0, days);
+        return ResponseEntity.ok(payment);
+    }
+
 }
--- hr-ms-worker/src/main/java/com/bmsti/hrmsworker/resources/WorkerResource.java
+++ hr-ms-worker/src/main/java/com/bmsti/hrmsworker/resources/WorkerResource.java
@@ -40,6 +40,12 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
+        /*try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
         logger.info("Port = " + environment.getProperty("local.server.port"));
 
         Optional<Worker> worker = workerRepository.findById(id);

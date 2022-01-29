5b720189873c81b3534834e1f3e913a08e5dfb1a
angelobms <angelobms@gmail.com>
2022-01-29 16:48:40 -0300

    Created hr-ms-config-server

---
+++ hr-ms-config-server/src/main/java/br/com/bmsti/hrmsconfigserver/HrMsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.bmsti.hrmsconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMsConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMsConfigServerApplication.class, args);
+    }
+
+}
---
+++ hr-ms-config-server/src/test/java/br/com/bmsti/hrmsconfigserver/HrMsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bmsti.hrmsconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrMsConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

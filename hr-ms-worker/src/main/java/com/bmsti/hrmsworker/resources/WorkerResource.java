1a34a45b3b8b65a4df7a9b026436d7e67e258a67
angelobms <angelobms@gmail.com>
2022-01-30 18:36:25 -0300

    Configured hr-ms-worker as a config server client

--- hr-ms-worker/src/main/java/com/bmsti/hrmsworker/resources/WorkerResource.java
+++ hr-ms-worker/src/main/java/com/bmsti/hrmsworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.bmsti.hrmsworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,9 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment environment;
 
@@ -31,7 +35,7 @@ public class WorkerResource {
         this.workerRepository = workerRepository;
     }
 
-    @GetMapping
+    @GetMapping()
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> workers = workerRepository.findAll();
         return ResponseEntity.ok(workers);
@@ -50,5 +54,11 @@ public class WorkerResource {
 
         Optional<Worker> worker = workerRepository.findById(id);
         return ResponseEntity.ok(worker.get());
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 }

0f05c07769a2c04483d3b3f96ff30b280cb3f978
angelobms <angelobms@gmail.com>
2022-01-31 20:18:51 -0300

    Added database seeding.

--- hr-ms-user/src/main/java/br/com/bmsti/hrmsuser/HrMsUserApplication.java
+++ hr-ms-user/src/main/java/br/com/bmsti/hrmsuser/HrMsUserApplication.java
@@ -1,15 +1,25 @@
 package br.com.bmsti.hrmsuser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrMsUserApplication {
+public class HrMsUserApplication implements CommandLineRunner {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(HrMsUserApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("BCRYPT = " + bCryptPasswordEncoder.encode("123456"));
+    }
 }

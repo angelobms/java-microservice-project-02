3a9a451f04b5124c345e92463a8ddbb54053d519
angelobms <angelobms@gmail.com>
2022-01-31 19:23:02 -0300

    Created hr-ms-user project

---
+++ hr-ms-user/src/main/java/br/com/bmsti/hrmsuser/HrMsUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.bmsti.hrmsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMsUserApplication.class, args);
+    }
+
+}
---
+++ hr-ms-user/src/test/java/br/com/bmsti/hrmsuser/HrMsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bmsti.hrmsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrMsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

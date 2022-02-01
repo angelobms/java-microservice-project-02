5bdbbb0257b12c58fe4ff3e8c4b12d087cda7306
angelobms <angelobms@gmail.com>
2022-01-31 21:46:02 -0300

    Created hr-ms-oauth project

---
+++ hr-ms-oauth/src/main/java/br/com/bmsti/hrmsoauth/HrMsOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.bmsti.hrmsoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMsOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMsOauthApplication.class, args);
+    }
+
+}

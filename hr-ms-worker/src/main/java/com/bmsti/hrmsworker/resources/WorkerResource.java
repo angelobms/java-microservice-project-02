73bc1fb9d2525341d132d8748533b20e72b41396
angelobms <angelobms@gmail.com>
2022-02-06 15:33:44 -0300

    Added db config server

--- hr-ms-worker/src/main/java/com/bmsti/hrmsworker/resources/WorkerResource.java
+++ hr-ms-worker/src/main/java/com/bmsti/hrmsworker/resources/WorkerResource.java
@@ -24,8 +24,8 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
+/*    @Value("${test.config}")
+    private String testConfig;*/
 
     @Autowired
     private Environment environment;
@@ -46,11 +46,11 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-        try {
+        /*try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
 
         logger.info("Port = " + environment.getProperty("local.server.port"));
 
@@ -58,9 +58,9 @@ public class WorkerResource {
         return ResponseEntity.ok(worker.get());
     }
 
-    @GetMapping(value = "/configs")
+    /*@GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
         logger.info("CONFIG = " + testConfig);
         return ResponseEntity.noContent().build();
-    }
+    }*/
 }

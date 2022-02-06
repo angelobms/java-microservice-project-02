3d57d8f56849323bec4d65384aec48b9b5c88143
angelobms <angelobms@gmail.com>
2022-02-06 11:09:54 -0300

    Configured Zuul authorization

---
+++ hr-ms-api-gateway-zuul/src/main/java/br/com/bmsti/hrmsapigatewayzuul/config/ResourceServerConfig.java
@@ -0,0 +1,38 @@
+package br.com.bmsti.hrmsapigatewayzuul.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private static final String ROLE_ADMIN = "ADMIN";
+    private static final String ROLE_OPERETOR = "OPERETOR";
+    private static final String[] PUBLIC = { "/hr-ms-oauth/oauth/token" };
+    private static final String[] OPERETOR = { "/hr-ms-worker/**" };
+    private static final String[] ADMIN = { "/hr-ms-payroll/**", "/hr-ms-user/**" };
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERETOR).hasAnyRole(ROLE_OPERETOR, ROLE_ADMIN)
+                .antMatchers(ADMIN).hasRole(ROLE_ADMIN)
+                .anyRequest().authenticated();
+    }
+}
